572c24196c145d45d6463c0d860f08fa1b47dadf
zachbwh <zachbwh@gmail.com>
2023-10-14 10:05:44 +1300

    chore: lint

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { GluestackUIProvider, Box } from "@gluestack-ui/themed";
+import { GluestackUIProvider, Box, Text } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import Auth from "./Auth";
 import { Session } from "@supabase/supabase-js";
@@ -6,22 +6,28 @@ import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-  
+      setSession(session);
+    });
+  }, []);
+
   return (
     <GluestackUIProvider config={config}>
-      <Box width="100%" height="100%" justifyContent="center" alignItems="center">
+      <Box
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        alignItems="center"
+      >
         {!session && <Auth />}
+        {session && <Text>Test2</Text>}
       </Box>
     </GluestackUIProvider>
   );
