bf542c9e3119ac908b5e54a2ed6efcf485e3763d
zachbwh <zachbwh@gmail.com>
2023-10-28 08:55:41 +1300

    add comments to posts

--- app/posts/[id]/index.tsx
+++ app/posts/[id]/index.tsx
@@ -1,12 +1,28 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { supabase } from "../../../lib/supabase";
 import { Image, StyleSheet, View } from "react-native";
-import { Heading } from "@gluestack-ui/themed";
+import {
+  Heading,
+  Text,
+  Input,
+  InputField,
+  Button,
+  ButtonText,
+} from "@gluestack-ui/themed";
 import { Stack, useLocalSearchParams } from "expo-router";
 
 type Post = {
   id: string;
   image_url: string;
+};
+
+type Comment = {
+  id: string;
+  post_id: string;
+  user_id: string;
+  parent_id: string | null;
+  created_at: string;
+  text: string;
 };
 
 const Post: FC = () => {
@@ -24,6 +40,51 @@ const Post: FC = () => {
       });
   }, []);
 
+  const [comments, setComments] = useState<Comment[]>([]);
+  useEffect(() => {
+    supabase
+      .from("comments")
+      .select("id, post_id, user_id, parent_id, created_at, text")
+      .eq("post_id", postId)
+      .then((data) => {
+        if (data.data) {
+          setComments(data.data as Comment[]);
+        }
+      });
+  }, []);
+
+  const [inputText, setInputText] = useState("");
+
+  const submit = useCallback(async () => {
+    const commentData: Omit<Comment, "id" | "user_id" | "created_at"> = {
+      post_id: postId as string,
+      parent_id: null,
+      text: inputText,
+    };
+    setComments([
+      ...comments,
+      {
+        id: "temp",
+        user_id: "temp",
+        created_at: new Date().toISOString(),
+        ...commentData,
+      },
+    ]);
+    setInputText("")
+    const { error: insertError, data: insertData } = await supabase
+      .from("comments")
+      .insert(commentData);
+    if (insertError) {
+      console.error("failed to upload comment data", {
+        insertError,
+        commentData,
+      });
+      // Handle error
+      return;
+    }
+    console.log("inserted comment into db", insertData);
+  }, [inputText, comments]);
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Cat" }} />
@@ -33,6 +94,23 @@ const Post: FC = () => {
           <Image source={{ uri: post.image_url }} style={styles.imagePreview} />
         </View>
       )}
+      <View style={styles.commentsContainer}>
+        {comments &&
+          comments.map((comment, index) => {
+            return <Text key={index}>{comment.text}</Text>;
+          })}
+      </View>
+      <Input>
+        <InputField type="text" value={inputText} onChangeText={setInputText} />
+        <Button
+          ml="auto"
+          onPress={() => {
+            submit();
+          }}
+        >
+          <ButtonText color="$white">Send</ButtonText>
+        </Button>
+      </Input>
     </View>
   );
 };
@@ -41,7 +119,7 @@ const styles = StyleSheet.create({
   container: {
     margin: 12,
     marginTop: 32,
-    flex: 1
+    flex: 1,
   },
   heading: {
     paddingHorizontal: 4,
@@ -50,14 +128,17 @@ const styles = StyleSheet.create({
     lineHeight: 40,
   },
   imageContainer: {
-    width: '100%',
-    margin: 'auto',
-    alignItems: 'center'
+    width: "100%",
+    margin: "auto",
+    alignItems: "center",
   },
   imagePreview: {
     aspectRatio: 9 / 16,
     width: "50%",
     borderRadius: 16,
+  },
+  commentsContainer: {
+    flex: 1,
   },
 });
 
