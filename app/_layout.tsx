377ebb0d7f8eaf7e137f264148dc473a7199fa3f
zachbwh <zachbwh@gmail.com>
2023-11-25 15:23:17 +1300

    poc android push notifications working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,10 @@ import { config } from "@gluestack-ui/config";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { usePushNotifications } from "../lib/usePushNotifications";
 
 const Layout: React.FC = () => {
+  const pn = usePushNotifications()
   return (
     <SafeAreaView style={styles.safeContainer}>
       <GluestackUIProvider config={config} colorMode="dark">
---
+++ lib/usePushNotifications.ts
@@ -0,0 +1,104 @@
+import { useEffect, useRef, useState } from "react";
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+export interface PushNotificationsState {
+  expoPushToken?: Notifications.ExpoPushToken;
+  notification?: Notifications.Notification
+}
+
+export const usePushNotifications = (): PushNotificationsState => {
+  const [expoPushToken, setExpoPushToken] = useState<Notifications.ExpoPushToken>();
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: Constants.expoConfig?.extra?.eas.projectId,
+        })
+      ).data;
+      console.log(token);
+
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: false,
+          shouldSetBadge: false,
+        }),
+      });
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      token && setExpoPushToken(token as unknown as Notifications.ExpoPushToken)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+
+        console.log(notification)
+        setNotification(notification);
+
+        // Notifications.scheduleNotificationAsync({
+        //     content: {
+        //       title: notification.request.content.title,
+        //       body: notification.request.content.body,
+        //     },
+        //     trigger: null,
+        //   });
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
+  }, []);
+
+  return { expoPushToken, notification };
+};
