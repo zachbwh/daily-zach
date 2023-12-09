4002b54567edf57c441148686d1fb766c2062e5e
zachbwh <zachbwh@gmail.com>
2023-12-09 15:34:39 +1300

    fix: login redirect, lint

--- app/index.tsx
+++ app/index.tsx
@@ -5,27 +5,29 @@ import { View, StyleSheet, Animated } from "react-native";
 import { useRouter } from "expo-router";
 
 const Index: FC = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    supabase.auth.getSession().then(response => {
-        if (response.data) {
-            console.log("found session, directing to /home!")
-            router.replace("/home/posts")
-        } else {
-            router.replace("/login")
-        }
-    })
-  }, [])
+    supabase.auth.getSession().then((response) => {
+      if (response.data.session) {
+        console.log("found session, directing to /home!");
+        router.replace("/home/posts");
+      } else {
+        router.replace("/login");
+      }
+    });
+  }, []);
 
   const spinAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.loop(Animated.timing(spinAnim, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true
-    })).start();
+    Animated.loop(
+      Animated.timing(spinAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      })
+    ).start();
   }, [spinAnim]);
 
   // Next, interpolate beginning and end values (in this case 0 and 1)
@@ -37,9 +39,7 @@ const Index: FC = () => {
   return (
     <View style={styles.container}>
       <Animated.View style={{ transform: [{ rotate: spin }] }}>
-          <Loader2Icon
-            style={styles.loadingIcon}
-          />
+        <Loader2Icon style={styles.loadingIcon} />
       </Animated.View>
     </View>
   );
