0ef3a99b134af1cdee63a6e2e8edaa95a6b574a8
zachbwh <zachbwh@gmail.com>
2023-12-09 21:45:24 +1300

    simplify loading animation

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect } from "react";
 import { supabase } from "../lib/supabase";
-import { Loader2Icon } from "lucide-react-native";
-import { View, StyleSheet, Animated } from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 
 const Index: FC = () => {
@@ -18,29 +17,9 @@ const Index: FC = () => {
     });
   }, []);
 
-  const spinAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.timing(spinAnim, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      })
-    ).start();
-  }, [spinAnim]);
-
-  // Next, interpolate beginning and end values (in this case 0 and 1)
-  const spin = spinAnim.interpolate<string>({
-    inputRange: [0, 1],
-    outputRange: ["0deg", "360deg"],
-  });
-
   return (
     <View style={styles.container}>
-      <Animated.View style={{ transform: [{ rotate: spin }] }}>
-        <Loader2Icon style={styles.loadingIcon} />
-      </Animated.View>
+      <ActivityIndicator color="#FFFFFF" />
     </View>
   );
 };
@@ -51,9 +30,6 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  loadingIcon: {
-    color: "white",
   },
 });
 
