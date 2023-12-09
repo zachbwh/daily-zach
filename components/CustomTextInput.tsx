f0731348aabf74f89869b0e66782ed94b2943645
zachbwh <zachbwh@gmail.com>
2023-12-09 20:30:16 +1300

    add custom text input

---
+++ components/CustomTextInput.tsx
@@ -0,0 +1,53 @@
+import { StyleProp, StyleSheet, TextInput, TextInputProps, View, ViewStyle } from "react-native";
+
+const CustomTextInput: React.FC<TextInputProps & { icon?: JSX.Element, wrapperStyle?: StyleProp<ViewStyle> }> = (
+  props
+) => {
+  return (
+    <View style={styles.inputWrapper}>
+      {props.icon && (<View style={styles.iconWrapper}>{props.icon}</View>)}
+      <TextInput
+        {...props}
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholderTextColor="#AAAAAA"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "stretch",
+    height: 48,
+    backgroundColor: "#222222",
+    borderWidth: 1,
+    borderColor: "#888888",
+    borderRadius: 8,
+    overflow: "hidden"
+  },
+  iconWrapper: {
+    backgroundColor: "#222222",
+    flexGrow: 0,
+    height: "100%",
+    justifyContent: "center",
+    paddingRight: 8,
+    paddingLeft: 8,
+  },
+  input: {
+    flex: 1,
+    flexGrow: 1,
+    paddingTop: 8,
+    paddingRight: 8,
+    paddingBottom: 8,
+    paddingLeft: 8,
+    color: "#FFFFFF",
+    backgroundColor: "#222222",
+    height: "100%"
+  },
+});
+
+
+export default CustomTextInput
