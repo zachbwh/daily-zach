34d24cdecab8fb952c4007b45a2d0ba221dcfe30
zachbwh <zachbwh@gmail.com>
2023-12-17 19:09:20 +1300

    improve request progress page, add animations

--- app/requests/RequestProgress.tsx
+++ app/requests/RequestProgress.tsx
@@ -1,32 +1,95 @@
-import { FC } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { FC, useEffect, useRef } from "react";
+import { View, StyleSheet, Text, Animated, Easing } from "react-native";
 import { RequestStatus } from "./types";
-import { Circle } from "lucide-react-native";
 
 const statusCopy: Record<RequestStatus, string> = {
-  CREATED: "Notification Sent",
-  DELIVERED: "Notification Delivered",
+  CREATED: "Sent",
+  DELIVERED: "Delivered",
   ACKNOWLEDGED: "Acknowledged",
   UPLOADING: "Uploading",
   COMPLETED: "Uploaded!",
 };
 
+const statusEmoji: Record<RequestStatus, string> = {
+  CREATED: "📤",
+  DELIVERED: "⏰",
+  ACKNOWLEDGED: "👍",
+  UPLOADING: "🚚",
+  COMPLETED: "✅",
+};
+
+const RequestProgressEntry: FC<{
+  emoji: string;
+  name: string;
+  inProgress: boolean;
+  completed: boolean;
+}> = ({ emoji, name, inProgress, completed }) => {
+  const completeAnimatedValue = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    console.log("completed ", { name, completed })
+    if (completed) {
+      Animated.timing(completeAnimatedValue, {
+        toValue: 1.0,
+        duration: 600,
+        easing: Easing.ease,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [completed]);
+  return (
+    <View style={styles.statusEntry}>
+      <Animated.Text
+        style={[
+          styles.progressText,
+          {
+            transform: [
+              {
+                scale: completeAnimatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1.2, 1.5],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        {emoji}
+      </Animated.Text>
+      <Animated.Text
+        style={[
+          styles.progressText,
+          {
+            color: completeAnimatedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: ["#CCCCCC", "#FFFFFF"],
+            }),
+          },
+        ]}
+      >
+        {name}
+      </Animated.Text>
+    </View>
+  );
+};
+
 const RequestProgress: FC<{
   requestStatus: RequestStatus;
 }> = ({ requestStatus }) => {
-    const statusIndex = Object.values(RequestStatus).findIndex((status) => requestStatus === status)
+  const statusIndex = Object.values(RequestStatus).findIndex(
+    (status) => requestStatus === status
+  );
   return (
     <View style={styles.container}>
       {Object.values(RequestStatus).map((statusValue, index) => {
-        const stepCompleted = statusIndex >= index
-        const stepColour = stepCompleted ? "#EEEEEE" : "#AAAAAA"
+        const stepCompleted = statusIndex >= index;
         return (
-          <View style={styles.statusEntry} key={statusValue}>
-            <Circle size={40} strokeWidth={2.5} color={stepColour} fill={stepCompleted ? stepColour : undefined} />
-            <Text style={[styles.progressText, (stepCompleted ? styles.completeText : undefined)]}>
-              {statusCopy[statusValue]}
-            </Text>
-          </View>
+          <RequestProgressEntry
+            emoji={statusEmoji[statusValue]}
+            name={statusCopy[statusValue]}
+            inProgress={false}
+            completed={stepCompleted}
+            key={statusValue}
+          />
         );
       })}
     </View>
@@ -37,7 +100,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 40,
-    paddingTop: 48
+    paddingTop: 48,
   },
   header: {
     color: "white",
@@ -51,17 +114,14 @@ const styles = StyleSheet.create({
   },
   statusEntry: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
   progressText: {
-    color: "#CCCCCC",
     fontWeight: "500",
     fontSize: 24,
-    paddingLeft: 16
+    paddingRight: 8,
+    paddingLeft: 8,
   },
-  completeText: {
-    color: "white"
-  }
 });
 
 export default RequestProgress;
