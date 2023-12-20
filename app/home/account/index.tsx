85f9c8f6cd357c5ed12edd9e852c0a3bc97b1661
zachbwh <zachbwh@gmail.com>
2023-12-20 19:27:04 +1300

    improve logout and request deletion flows

--- app/home/account/index.tsx
+++ app/home/account/index.tsx
@@ -1,12 +1,62 @@
 import { FC } from "react";
 import { supabase } from "../../../lib/supabase";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 import Button from "./Button";
 import { LogOut, Trash, User } from "lucide-react-native";
-import { useRouter } from "expo-router";
+import { router } from "expo-router";
+
+const logout = () => {
+  Alert.alert("Logout", "Are you sure you want to logout?", [
+    { text: "Cancel" },
+    {
+      text: "Logout",
+      onPress: () => {
+        supabase.auth.signOut().then((response) => {
+          if (response.error) {
+            console.log("error logging out, please try again");
+          } else {
+            router.replace("/login");
+          }
+        });
+      },
+    },
+  ]);
+};
+
+const deleteAccount = () => {
+  Alert.alert(
+    "Request Account Deletion",
+    "Are you sure you want to delete your account?",
+    [
+      { text: "Cancel" },
+      {
+        text: "Delete",
+        onPress: async () => {
+          const { error: insertError, data: insertData } = await supabase
+            .from("deletion_requests")
+            .insert({});
+          if (insertError) {
+            console.error("failed to submit deletion request", {
+              insertError,
+              insertData,
+            });
+            // Handle error
+            return;
+          }
+          supabase.auth.signOut().then((response) => {
+            Alert.alert(
+              "Deletion Pending",
+              "Please reach out to customer support if you wish to prevent deletion."
+            );
+            router.replace("/signup");
+          });
+        },
+      },
+    ]
+  );
+};
 
 const Account: FC = () => {
-  const router = useRouter()
   return (
     <View style={styles.container}>
       <View style={styles.tile}>
@@ -19,14 +69,7 @@ const Account: FC = () => {
         />
         <Button
           onPress={() => {
-            console.log("TEST");
-            supabase.auth.signOut().then(response => {
-              if (response.error) {
-                  console.log("error logging out, please try again")
-              } else {
-                  router.replace("/login")
-              }
-          })
+            logout();
           }}
           label="Logout"
           icon={<LogOut style={styles.icon} />}
@@ -35,7 +78,7 @@ const Account: FC = () => {
       <View style={styles.tile}>
         <Button
           onPress={() => {
-            console.log("TEST");
+            deleteAccount();
           }}
           label="Delete Account"
           icon={<Trash style={styles.icon} />}
