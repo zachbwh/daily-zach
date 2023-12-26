b939e5f47deb6dd5392348e5ecc790717bee0795
zachbwh <zachbwh@gmail.com>
2023-12-27 08:00:01 +1300

    user hooks

---
+++ lib/react-query/user.ts
@@ -0,0 +1,42 @@
+import { supabase } from "@lib/supabase";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import queryClient from "./client";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+
+export type User = {
+  id: string;
+  user_id: string;
+  display_name: string;
+  profile_image_url?: string;
+};
+
+export const useUser = () => {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const { data } = await supabase
+        .from("users")
+        .select("id, user_id, display_name, profile_image_url")
+        .limit(1);
+      return data![0];
+    },
+  });
+};
+
+export const useUpdateUser = () => {
+  return useMutation({
+    mutationFn: async (
+      user: Partial<Omit<User, "id" | "user_id" | "created_at">>
+    ) => {
+      return supabase
+        .from("users")
+        .insert(user)
+        .select("id, user_id, display_name, profile_image_url");
+    },
+    onSuccess: (result, variables, context) => {
+      console.log("Sucess Response", result.data);
+      queryClient.setQueryData(["comments"], (old: User) => result.data![0]);
+    },
+  });
+};
