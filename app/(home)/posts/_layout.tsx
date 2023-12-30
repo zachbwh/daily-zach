f9fd7a03f72b43cc0d7b508870c0d31bf985bd7a
zachbwh <zachbwh@gmail.com>
2023-12-30 14:34:09 +1300

    add account icon to posts header

--- app/(home)/posts/_layout.tsx
+++ app/(home)/posts/_layout.tsx
@@ -1,6 +1,14 @@
 import { FC } from "react";
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { useCurrentUser } from "@lib/react-query/user";
+import ProfileImage from "@components/ProfileImage";
 const Posts: FC<{ children: React.ReactElement }> = ({ children }) => {
+  const { data: user } = useCurrentUser();
+  const imageSource = user?.profile_image_url
+    ? { uri: user.profile_image_url }
+    : null;
+
   return (
     <Stack
       screenOptions={{
@@ -9,9 +17,27 @@ const Posts: FC<{ children: React.ReactElement }> = ({ children }) => {
         },
         headerTintColor: "#fff",
         headerTitle: "Zachs",
+        headerRight: (props) => {
+          if (!props.canGoBack) {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  router.push("/account");
+                }}
+                style={styles.profileImageWrapper}
+              >
+                <ProfileImage imageSource={imageSource} />
+              </TouchableOpacity>
+            );
+          }
+        },
       }}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  profileImageWrapper: { height: 32, width: 32 },
+});
 
 export default Posts;
