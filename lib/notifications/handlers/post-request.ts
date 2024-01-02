80b1e65f4b627dd4bd8655b10155c1e646442de2
zachbwh <zachbwh@gmail.com>
2024-01-02 13:46:12 +1300

    remove http request from handle post request (due to 3 second timeout

--- lib/notifications/handlers/post-request.ts
+++ lib/notifications/handlers/post-request.ts
@@ -1,5 +1,3 @@
-import { RequestStatus } from "@app/requests/types";
-import { supabase } from "@lib/supabase";
 import * as Notifications from "expo-notifications";
 import {
   AndroidNotificationPriority,
@@ -9,18 +7,6 @@ import {
 const handlePostRequest = async (
   notification: Notifications.Notification
 ): Promise<NotificationBehavior> => {
-  const requestId = notification.request.content.data["request_id"];
-
-  try {
-    // await supabase
-    //   .from("post_requests")
-    //   .update({ status: RequestStatus.DELIVERED })
-    //   .eq("id", requestId)
-    //   .select("id, created_at, requestor_id, status, post_id");
-  } catch (error) {
-    console.error("Failure updating post request status", error);
-  }
-
   return {
     shouldShowAlert: true,
     shouldPlaySound: true,
