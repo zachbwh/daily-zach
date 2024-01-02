780172700314739c0ec72481b05310582d10add7
zachbwh <zachbwh@gmail.com>
2024-01-02 13:13:15 +1300

    wire up camera component to post request flow

--- app/(home)/camera/index.tsx
+++ app/(home)/camera/index.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ViewFinder from "@components/Viewfinder";
 import { ImageResult } from "expo-image-manipulator";
 import { uploadPictureToBucket } from "@lib/uploadPictureToBucket";
 import { useInsertPost } from "@lib/react-query/posts";
+import { router, useLocalSearchParams } from "expo-router";
+import { useUpdatePostRequest } from "@lib/react-query/post-request";
+import { RequestStatus } from "@app/requests/types";
 
 const Camera: React.FC = () => {
-  const { mutate } = useInsertPost();
+  const data = useLocalSearchParams();
+  const postRequestId = data["postRequestId"];
+  const { mutate: updatePostRequest } = useUpdatePostRequest();
+  useEffect(() => {
+    if (typeof postRequestId === "string") {
+      updatePostRequest({
+        requestId: postRequestId,
+        data: {
+          status: RequestStatus.ACKNOWLEDGED,
+        },
+      });
+    }
+  }, [data]);
+  const { mutate: insertPost, data: postData } = useInsertPost();
+  useEffect(() => {
+    if (typeof postRequestId === "string" && postData && postData.data) {
+      const insertedPost = postData.data[0];
+      updatePostRequest({
+        requestId: postRequestId,
+        data: {
+          status: RequestStatus.COMPLETED,
+          post_id: insertedPost.id,
+        },
+      });
+      router.replace(`/posts/${insertedPost.id}`);
+    }
+  }, [postRequestId, postData]);
   async function uploadImage(picture: ImageResult) {
     try {
+      if (typeof postRequestId === "string") {
+        await updatePostRequest({
+          requestId: postRequestId,
+          data: {
+            status: RequestStatus.UPLOADING,
+          },
+        });
+      }
       const publicUrl = await uploadPictureToBucket(picture, "posts");
-      mutate({
+      await insertPost({
         image_url: publicUrl,
       });
     } catch (e) {
