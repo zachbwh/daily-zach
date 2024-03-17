640ac5ab84316ce2df6673aa0a1793bac7b215ad
zachbwh <zachbwh@gmail.com>
2024-03-17 11:25:39 -0500

    add account deletion tyepform to web

---
+++ website/app/posts/account/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default async function AccountDeletionForm() {
+  return (
+    <main className="min-h-screen w-full bg-[#000000] h-screen">
+      <div
+        className="h-full [&>*]:min-h-full"
+        data-tf-live="01HS6JS4ESX9698XK3QD0SHBNC"
+      ></div>
+      <script src="//embed.typeform.com/next/embed.js"></script>
+    </main>
+  );
+}
